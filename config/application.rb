da0541eab3331ef4cd1616841167b6c0a771d4f0
Justin Lynch <justinmatthewlynch@gmail.com>
2013-11-01 13:19:48 -0400

    push to heroku

--- config/application.rb
+++ config/application.rb
@@ -78,5 +78,6 @@ module Htcgen
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false 
   end
 end
