70ffdb41f1b7640f60e88c4c70c0ef19e6a08dc9
Justin Lynch <justinmatthewlynch@gmail.com>
2013-12-28 21:03:41 -0500

    dfewf

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'rolify'
 gem 'simple_form'
 gem 'mechanize'
 gem 'nokogiri'
+gem 'rubyzip'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/controllers/mech_controller.rb
+++ app/controllers/mech_controller.rb
@@ -1,16 +1,29 @@
 class MechController < ApplicationController
+   require 'rubygems'
+   require 'zip'
+
    def export
    end
 
    def grasp
-   	  email = params[:exportemail]
-      password = params[:exportpassword]
+   	  path = Rails.root.join('tmp')
+   	  system("wget -p -k -P #{path} http://htcgen.herokuapp.com/site/texas/blood-testing/abilene-tx.html")
 
+      filename = 'website.zip'
+      temp_file = Tempfile.new(filename)
+      directory = Rails.root.join('tmp/htcgen.herokuapp.com/site/texas/blood-testing/')
 
+      begin
+        Zip::OutputStream.open(temp_file) { |zos| }
+      	Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+          zip.add('abilene-tx.html', directory + 'abilene-tx.html')
+        end
 
-      
-      creds = email + password
-      outy = creds.to_s
-      send_data(outy, :type => 'text/plain', :filename => "mySummary.txt" )
+        zip_data = File.read(temp_file.path)
+        send_data(zip_data, :type => 'application/zip', :filename => filename)
+      ensure
+      	temp_file.close
+        temp_file.unlink
+      end
    end
 end
