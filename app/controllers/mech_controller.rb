a68e90100633527e3de10eea4e59c8022852c0e4
Justin Lynch <justinmatthewlynch@gmail.com>
2013-12-30 04:50:44 -0500

    chnages

--- app/controllers/mech_controller.rb
+++ app/controllers/mech_controller.rb
@@ -1,6 +1,6 @@
 class MechController < ApplicationController
    require 'rubygems'
-   require 'zip'
+   require 'mechanize'
 
    def export
    end
@@ -13,27 +13,29 @@ class MechController < ApplicationController
       # temp_file = Tempfile.new(filename)
       # directory = Rails.root.join('tmp/htcgen.herokuapp.com/site/texas/blood-testing/')
 
-      path = Rails.root.join('tmp')
-      system("wget -p -k -r -P #{path} http://htcgen.herokuapp.com/site/texas/blood-testing/abilene-tx.html")
 
-      filename = 'website.zip'
-      temp_file = Tempfile.new(filename)
-      directory = Rails.root.join('tmp/htcgen.herokuapp.com')
 
-      begin
-        Zip::OutputStream.open(temp_file) { |zos| }
-      	Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
-          Dir.glob("**/*").each do |file|
-            zip.add file, file
-          end
-          # zip.add('abilene-tx.html', directory + 'abilene-tx.html')
-        end
+      # path = Rails.root.join('tmp')
+      # system("wget -p -k -r -P #{path} http://htcgen.herokuapp.com/site/texas/blood-testing/abilene-tx.html")
 
-        zip_data = File.read(temp_file.path)
-        send_data(zip_data, :type => 'application/zip', :filename => filename)
-      ensure
-      	temp_file.close
-        temp_file.unlink
-      end
+      # filename = 'website.zip'
+      # temp_file = Tempfile.new(filename)
+      # directory = Rails.root.join('tmp/htcgen.herokuapp.com')
+
+      # begin
+      #   Zip::OutputStream.open(temp_file) { |zos| }
+      # 	Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+      #     Dir.glob("**/*").each do |file|
+      #       zip.add file, file
+      #     end
+      #     # zip.add('abilene-tx.html', directory + 'abilene-tx.html')
+      #   end
+
+      #   zip_data = File.read(temp_file.path)
+      #   send_data(zip_data, :type => 'application/zip', :filename => filename)
+      # ensure
+      # 	temp_file.close
+      #   temp_file.unlink
+      # end
    end
 end
