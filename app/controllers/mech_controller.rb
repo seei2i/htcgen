72b89b74f263d1f0987ffbf53e3713121f306da3
Justin Lynch <justinmatthewlynch@gmail.com>
2013-12-30 05:38:45 -0500

    fefw

--- app/controllers/mech_controller.rb
+++ app/controllers/mech_controller.rb
@@ -6,8 +6,35 @@ class MechController < ApplicationController
    end
 
    def grasp
-   	  # path = Rails.root.join('tmp')
-   	  # system("wget -p -k -P #{path} http://htcgen.herokuapp.com/site/texas/blood-testing/abilene-tx.html")
+      filename = 'website.zip'
+      temp_file = Tempfile.new(filename)
+
+      agent = Mechanize.new
+      agent.pluggable_parser.html = Mechanize::FileSaver
+      pager = agent.get('http://htcgen.herokuapp.com/site/alabama.html')
+
+      # send_file(pager, :type => 'text/html', :filename => "alabama.html" )
+
+
+      begin
+         Zip::OutputStream.open(temp_file) { |zos| }
+         Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
+            Dir.glob("**/*").each do |file|
+            zip.add file, file
+          end
+          # zip.add('abilene-tx.html', directory + 'abilene-tx.html')
+        end
+
+        zip_data = File.read(temp_file.path)
+        send_data(zip_data, :type => 'application/zip', :filename => filename)
+      ensure
+        temp_file.close
+        temp_file.unlink
+      end
+
+
+   	# path = Rails.root.join('tmp')
+   	# system("wget -p -k -P #{path} http://htcgen.herokuapp.com/site/texas/blood-testing/abilene-tx.html")
 
       # filename = 'website.zip'
       # temp_file = Tempfile.new(filename)
