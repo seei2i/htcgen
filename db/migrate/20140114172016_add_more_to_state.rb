e26fd9d883299e3a467496766753c44f0933d608
Justin Lynch <justinmatthewlynch@gmail.com>
2014-01-14 13:34:09 -0500

    message

--- db/migrate/20140114172016_add_more_to_state.rb
+++ db/migrate/20140114172016_add_more_to_state.rb
@@ -1,19 +1,19 @@
 class AddMoreToState < ActiveRecord::Migration
   def change
   	add_column :states, :h1sub1,        :string
-  	add_column :states, :h1sub1section, :string
+  	add_column :states, :h1sub1section, :text
   	add_column :states, :h1sub2,        :string
-  	add_column :states, :h1sub2section, :string
+  	add_column :states, :h1sub2section, :text
   	add_column :states, :h1sub3,        :string
-  	add_column :states, :h1sub3section, :string
+  	add_column :states, :h1sub3section, :text
   	add_column :states, :h1sub4,        :string
-  	add_column :states, :h1sub4section, :string
+  	add_column :states, :h1sub4section, :text
   	add_column :states, :h1sub5,        :string
-  	add_column :states, :h1sub5section, :string
+  	add_column :states, :h1sub5section, :text
   	add_column :states, :h1sub6,        :string
-  	add_column :states, :h1sub6section, :string
+  	add_column :states, :h1sub6section, :text
   	add_column :states, :hlocations,    :string
   	add_column :states, :h3, :string
-  	add_column :states, :h3section, :string
+  	add_column :states, :h3section, :text
   end
 end
